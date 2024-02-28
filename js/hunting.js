825ea04d62b0b9236f3c174994ba0c543377f564
kibriarobin97 <kibriarobin97@gmail.com>
2024-02-28 09:40:51 +0600

    added phone

--- js/hunting.js
+++ js/hunting.js
@@ -1,7 +1,66 @@
-const loadPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
-    console.log(phones);
+    displayPhones(phones);
 }
-loadPhone();
+
+
+const displayPhones = phones => {
+
+    const phoneContainer = document.getElementById('phone-container');
+    phoneContainer.textContent = '';
+
+    const showAllContainer = document.getElementById('show-all-container');
+
+    if (phones.length > 12) {
+        showAllContainer.classList.remove('hidden');
+    }
+    else {
+        showAllContainer.classList.add('hidden');
+    }
+
+    phones = phones.slice(0, 12);
+
+    phones.forEach(phone => {
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card bg-base-100 p-4 shadow-xl`;
+        phoneCard.innerHTML = `
+        <figure><img src="${phone.image}"
+            alt="phone" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">${phone.phone_name}</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+                <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        `;
+        phoneContainer.appendChild(phoneCard);
+
+    });
+    toggleLoadingSpinner(false);
+}
+
+
+const handleSearch = () => {
+    toggleLoadingSpinner(true);
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+    loadPhone(searchText);
+}
+
+
+const toggleLoadingSpinner = (isLoading) => {
+    const loadingSpinner = document.getElementById('loading-spinner');
+    if (isLoading) {
+        loadingSpinner.classList.remove('hidden');
+    }
+    else{
+        loadingSpinner.classList.add('hidden');
+    }
+}
+
+// loadPhone();
